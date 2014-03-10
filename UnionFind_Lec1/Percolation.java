b681685f40e9ee0e2ef9c3324f8a8d4e4f431858
rama7777 <strongfocus15@gmail.com>
2014-03-10 06:05:51 -0700

    Added top and bottom nodes

--- UnionFind_Lec1/Percolation.java
+++ UnionFind_Lec1/Percolation.java
@@ -3,17 +3,32 @@
 public class Percolation
 {
  boolean [][] site;
- int topVirtualNode, bottomVirtualNode;
+ int [][]topVirtualNode, bottomVirtualNode;
  
  // Initialize all sites as blocked 
  
  public Percolation(int N)
  {
   site = new boolean[N+1][N+1];
+  topVirtualNote = new boolean [1][N+1];
+  bottomVirtualNote = new boolean [1][N+1];
 
   for(int i=1; i<=N; i++)
    for (int j=1; j<=N; j++)
     site[i][j] = false;
+
+  for(int i=1; i<=N; i++)
+  {
+   	topVirtualNode[0][i] = true;
+	site[0][i] = true; // Open up top row to connect to top virtual node
+  }
+
+  for(int i=1; i<=N; i++)
+  {
+   	bottomVirtualNode[0][i] = true;
+	site[N][i] = true; // Open up bottom row to connecto to bottom virtual node
+  }
+  
  }
 
  public void open(int i, int j)
