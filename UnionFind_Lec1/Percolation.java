3f33e487ce552e0c2903e3083eec7b69937bc38a
rama7777 <strongfocus15@gmail.com>
2014-03-10 05:24:43 -0700

    Initial version of Percolation or rather just interface

--- UnionFind_Lec1/Percolation.java
+++ UnionFind_Lec1/Percolation.java
@@ -2,42 +2,46 @@
 
 public class Percolation
 {
-	boolean [][] site;
+ boolean [][] site;
+ int topVirtualNode, bottomVirtualNode;
+ 
+ // Initialize all sites as blocked 
+ 
+ public Percolation(int N)
+ {
+  site = new boolean[N+1][N+1];
 
-	// Initialize all sites as blocked 
-	
-	public Percolation(int N)
-	{
-		site = new boolean[N+1][N+1];
+  for(int i=1; i<=N; i++)
+   for (int j=1; j<=N; j++)
+    site[i][j] = false;
+ }
 
-		for(int i=1; i<=N; i++)
-			for (int j=1; j<=N; j++)
-				site[i][j] = false;
-	}
+ public void open(int i, int j)
+ {
+     
+ }
 
-	public void open(int i, int j)
-	{
-	}
+ public boolean isOpen(int i, int j)
+ {
+  return site[i][j];
+ }
 
-	public boolean isOpen(int i, int j)
-	{
-	}
+ public boolean isFull(int i, int j)
+ {
+     // TODO: Replace it with code to find if site is full
+     return site[i][j];
+ }
 
-	public boolean isFull(int i, int j)
-	{
-	}
+ public boolean percolates()
+ {
+     // TODO: Replace it with code to find if system percolates
+     return false;
+ }
 
-	public boolean percolates()
-	{
-	}
-
-	public static void main(String []args)
-	{
-		StdOut.println("Enter the number of rows/cols");
-		int N = StdIn.getInt();
-	}
-
-
-
+ public static void main(String []args)
+ {
+  StdOut.println("Enter the number of rows/cols");
+  int N = StdIn.readInt();
+ }
 
 }
